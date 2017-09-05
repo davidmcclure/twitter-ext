90d14f846ea8c1c36d8a83a1e4b1430eed78ec9e
David McClure <davidwilliammcclure@gmail.com>
2017-09-05 12:12:40 -0400

    ModelMeta, Model.

--- twitter.py
+++ twitter.py
@@ -1,6 +1,7 @@
 
 
 from collections import namedtuple
+from pyspark.sql import SparkSession, types as T
 
 
 class ModelMeta(type):
@@ -8,12 +9,14 @@ class ModelMeta(type):
     def __new__(meta, name, bases, dct):
         """Generate a namedtuple from the `schema` class attribute.
         """
-        Row = namedtuple(name, dct['schema'].names)
+        if isinstance(dct.get('schema'), T.StructType):
 
-        # By default, default all fields to None.
-        Row.__new__.__defaults__ = (None,) * len(Row._fields)
+            Row = namedtuple(name, dct['schema'].names)
 
-        bases = (Row, Model)
+            # By default, default all fields to None.
+            Row.__new__.__defaults__ = (None,) * len(Row._fields)
+
+            bases = (Row,)
 
         return super().__new__(meta, name, bases, dct)
 
