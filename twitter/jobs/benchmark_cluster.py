44353e71b99e92c416a8671e5dc04ad84ad1e219
David McClure <davidwilliammcclure@gmail.com>
2017-09-27 10:25:23 -0400

    Add benchmark job.

---
+++ twitter/jobs/benchmark_cluster.py
@@ -0,0 +1,21 @@
+
+
+import time
+
+from twitter.utils import get_spark
+
+
+def work(i):
+    time.sleep(1)
+    return i + 1
+
+
+def main(sc, spark):
+    sc, spark = get_spark()
+    data = sc.parallelize(range(36))
+    result = data.map(work).collect()
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
