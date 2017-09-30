542ecd9c499a5b27be0969edea008e50c3b72bba
David McClure <davidwilliammcclure@gmail.com>
2017-09-30 14:09:15 -0400

    Ready for Gnip adapter.

--- code/twitter/jobs/load_tweets.py
+++ code/twitter/jobs/load_tweets.py
@@ -1,7 +1,7 @@
 
 
 import click
-import bz2
+import gzip
 import ujson
 
 from twitter import fs
@@ -9,33 +9,29 @@ from twitter.utils import get_spark
 from twitter.models import Tweet
 
 
-def parse_minute(path):
+def parse_segment(path):
     """Parse a raw minute file.
     """
-    with bz2.open(fs.read(path)) as fh:
+    with gzip.open(fs.read(path)) as fh:
         for line in fh.readlines():
 
             raw = ujson.loads(line)
 
-            if 'delete' in raw:
-                continue
-
-            yield Tweet.from_api_json(raw)
+            if raw['verb'] == 'post':
+                yield Tweet.from_gnip_json(raw)
 
 
 @click.command()
-@click.option('--src', default='data/twitter-ia')
+@click.option('--src', default='data/twitter-lsm')
 @click.option('--dest', default='data/tweets.parquet')
 def main(src, dest):
     """Ingest tweets.
     """
     sc, spark = get_spark()
 
-    paths = sc.parallelize(fs.scan(src, '\.json'))
+    paths = sc.parallelize(fs.scan(src, '\.json.gz'))
 
-    rows = paths.flatMap(parse_minute)
-
-    df = spark.createDataFrame(rows, Tweet.schema)
+    df = paths.flatMap(parse_segment).toDF(Tweet.schema)
 
     df.write.mode('overwrite').parquet(dest)
 
--- code/twitter/models.py
+++ code/twitter/models.py
@@ -28,51 +28,15 @@ class Model(metaclass=ModelMeta):
 class Tweet(Model):
 
     schema = T.StructType([
-
         T.StructField('id', T.StringType(), nullable=False),
-        T.StructField('text', T.StringType()),
-        T.StructField('timestamp_ms', T.StringType()),
-        T.StructField('lang', T.StringType()),
-        T.StructField('source', T.StringType()),
-        T.StructField('place', T.StringType()),
-
-        T.StructField('user', T.StructType([
-            T.StructField('screen_name', T.StringType()),
-            T.StructField('name', T.StringType()),
-            T.StructField('description', T.StringType()),
-            T.StructField('location', T.StringType()),
-            T.StructField('followers_count', T.IntegerType()),
-            T.StructField('statuses_count', T.IntegerType()),
-            T.StructField('url', T.StringType()),
-            T.StructField('lang', T.StringType()),
-        ])),
-
     ])
 
     @classmethod
-    def from_api_json(cls, json):
-        """Make a row from the raw API JSON.
+    def from_gnip_json(cls, json):
+        """Make a row from raw Gnip JSON.
         """
         return cls(
-
-            id=json['id_str'],
-            text=json['text'],
-            timestamp_ms=json['timestamp_ms'],
-            lang=json['lang'],
-            source=json['source'],
-            place=json['place'],
-
-            user=dict(
-                screen_name=json['user']['screen_name'],
-                name=json['user']['name'],
-                description=json['user']['description'],
-                location=json['user']['location'],
-                followers_count=json['user']['followers_count'],
-                statuses_count=json['user']['statuses_count'],
-                url=json['user']['url'],
-                lang=json['user']['lang'],
-            ),
-
+            id=json['id'],
         )
 
 
