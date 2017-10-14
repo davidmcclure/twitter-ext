7dff6f1869e866bf0573709089284188a8b3093f
David McClure <davidwilliammcclure@gmail.com>
2017-10-14 16:13:25 -0400

    Add ext_token_month_minute_counts.

---
+++ code/twitter/jobs/ext_token_month_minute_counts.py
@@ -0,0 +1,55 @@
+
+
+import click
+
+from wordfreq import top_n_list
+
+from twitter.utils import get_spark
+from twitter.models import Tweet
+
+
+whitelist = set(top_n_list('en', 10000))
+
+
+def count_tokens(tweet):
+    """Generate (token, month, minute) keys.
+    """
+    for token in tweet.tokens():
+        if token in whitelist:
+
+            month = tweet.posted_time.month
+            minute = tweet.posted_time.minute
+
+            yield (token, month, minute), 1
+
+
+def flatten_row(row):
+    (token, month, minute), count = row
+    return token, month, minute, count
+
+
+@click.command()
+@click.option('--src', default='data/tweets.parquet')
+@click.option('--dest', default='data/token-month-minute-counts.json')
+def main(src, dest):
+    """Extract (token, month, minute, count) tuples.
+    """
+    sc, spark = get_spark()
+
+    tweets = spark.read.parquet(src)
+
+    counts = tweets.rdd \
+        .map(Tweet.from_rdd) \
+        .filter(lambda t: t.actor.language == 'en') \
+        .flatMap(count_tokens) \
+        .reduceByKey(lambda a, b: a + b) \
+        .map(flatten_row) \
+        .toDF(('token', 'month', 'minute', 'count'))
+
+    counts.write \
+        .mode('overwrite') \
+        .json(dest)
+
+
+if __name__ == '__main__':
+    main()
