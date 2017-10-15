c0a543ac6383e53b947dadf3af365faa6731632f
David McClure <davidwilliammcclure@gmail.com>
2017-10-15 12:08:13 -0400

    More sophisticated year matching.

--- code/twitter/jobs/ext_year_candidates.py
+++ code/twitter/jobs/ext_year_candidates.py
@@ -6,13 +6,16 @@ import re
 from twitter.utils import get_spark
 
 
-def match_years(text, padding=30):
-    """Match 4-digit years.
-    """
-    for match in re.finditer('19[0-9]{2}', text):
+YEAR_PATTERN = '([^0-9a-z_]|^)(?P<year>1[5-9][0-9]{2})([^0-9a-z_]|$)'
 
-        c1 = match.start()
-        c2 = match.end()
+
+def match_years(text, padding=30):
+    """Match 4-digit years, 1500-1999.
+    """
+    for match in re.finditer(YEAR_PATTERN, text):
+
+        c1 = match.start('year')
+        c2 = match.end('year')
 
         c1p = max(c1-padding, 0)
         c2p = min(c2+padding, len(text))
@@ -20,12 +23,12 @@ def match_years(text, padding=30):
         prefix = text[c1p:c1]
         suffix = text[c2:c2p]
 
-        yield prefix, match.group(0), suffix
+        yield prefix, match.group('year'), suffix
 
 
 @click.command()
 @click.option('--src', default='data/tweets.parquet')
-@click.option('--dest', default='data/years.csv')
+@click.option('--dest', default='data/years.json')
 def main(src, dest):
     """Extract 4-digit year candidates.
     """
@@ -34,14 +37,13 @@ def main(src, dest):
     tweets = spark.read.parquet(src)
 
     matches = tweets.rdd \
-        .filter(lambda t: t.lang == 'en') \
-        .flatMap(lambda t: match_years(t.text)) \
+        .filter(lambda t: t.actor.language == 'en') \
+        .flatMap(lambda t: match_years(t.body)) \
         .toDF(('prefix', 'year', 'suffix'))
 
     matches.write \
         .mode('overwrite') \
-        .option('header', 'true') \
-        .csv(dest)
+        .json(dest)
 
 
 if __name__ == '__main__':
