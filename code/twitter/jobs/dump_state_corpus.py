0825f49e883b0ae1bf6baf9f5ed595496a793846
David McClure <davidwilliammcclure@gmail.com>
2017-12-03 23:28:39 -0500

    Add dump_state_corpus.

---
+++ code/twitter/jobs/dump_state_corpus.py
@@ -0,0 +1,42 @@
+
+
+import click
+import re
+
+from pyspark.sql import Row
+
+from twitter import fs
+from twitter.utils import get_spark, try_or_none
+from twitter.models import GeoTweet
+
+
+def clean_tweet(text):
+    """Remove links, mentions, and hashtags.
+    """
+    text = re.sub('(#|@|http)\S+', '', text)
+    return text.lower()
+
+
+@click.command()
+@click.argument('states', nargs=-1)
+@click.option('--src', default='/data/geo-tweets.parquet')
+@click.option('--dest', default='/data/corpus.txt')
+def main(states, src, dest):
+    """Dump state tweets for glove.
+    """
+    sc, spark = get_spark()
+
+    tweets = spark.read.parquet(src)
+
+    texts = (
+        tweets.filter(tweets.state.isin(set(states)))
+        .select('body')
+        .rdd.map(lambda r: Row(body=clean_tweet(r['body'])))
+        .toDF()
+    )
+
+    texts.write.text(dest)
+
+
+if __name__ == '__main__':
+    main()
