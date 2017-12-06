285618feccd858411678d699105cb4a1c3aeb888
David McClure <davidwilliammcclure@gmail.com>
2017-12-05 22:39:20 -0500

    States or regions.

--- code/twitter/jobs/dump_state_corpus.py
+++ code/twitter/jobs/dump_state_corpus.py
@@ -6,21 +6,30 @@ import re
 from pyspark.sql import Row
 
 from twitter import fs
-from twitter.utils import get_spark, try_or_none, clean_tweet
+from twitter.utils import get_spark, try_or_none, clean_tweet, read_yaml
 from twitter.models import GeoTweet
+
+
+regions = read_yaml(__file__, 'regions.yml')
 
 
 @click.command()
 @click.argument('states', nargs=-1)
+@click.option('--region')
 @click.option('--src', default='/data/geo-tweets.parquet')
 @click.option('--dest', default='/data/corpus.txt')
 @click.option('--partitions', default=100)
-def main(states, src, dest, partitions):
+def main(states, region, src, dest, partitions):
     """Dump state tweets for glove.
     """
     sc, spark = get_spark()
 
     tweets = spark.read.parquet(src)
+
+    if region:
+        states = regions[region]
+
+    print(states)
 
     texts = (
         tweets.filter(tweets.state.isin(set(states)))
