f977a0e5c068da8e8137bb2ebabd0d9b9029ce96
David McClure <davidwilliammcclure@gmail.com>
2018-02-08 20:17:38 -0500

    Add count_words job.

---
+++ code/twitter/jobs/count_words.py
@@ -0,0 +1,29 @@
+
+
+import click
+
+from operator import add
+
+from twitter.spark import spark
+from twitter.models import Tweet
+
+
+@click.command()
+@click.option('--src', default='/data/tweets.parquet')
+def main(src):
+    """Get word count for all tweets.
+    """
+    df = spark.read.parquet(src)
+
+    count = (
+        df.rdd
+        .map(Tweet.from_rdd)
+        .map(lambda t: len(t.tokens()))
+        .reduce(add)
+    )
+
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
