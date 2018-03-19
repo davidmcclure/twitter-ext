bbf7767fdc2a76a09d5b84f2be0976ebc19efccf
David McClure <davidwilliammcclure@gmail.com>
2018-03-19 00:56:17 -0400

    Fix DF creation.

--- code/twitter/jobs/dump_midwest.py
+++ code/twitter/jobs/dump_midwest.py
@@ -2,6 +2,9 @@
 
 import click
 
+from pyspark.sql.functions import countDistinct
+from pyspark.sql import Row
+
 from twitter.utils import get_spark, clean_tweet
 
 
@@ -26,7 +29,7 @@ states = [
 @click.option('--cities_dest', default='s3a://twitter-dev/midwest/cities.csv')
 @click.option('--rural_dest', default='s3a://twitter-dev/midwest/rural.txt')
 @click.option('--urban_dest', default='s3a://twitter-dev/midwest/urban.txt')
-def main(src, cities_dest, rural_dest):
+def main(src, cities_dest, rural_dest, urban_dest):
     """Dump midwest urban + rural corpora.
     """
     sc, spark = get_spark()
@@ -47,20 +50,20 @@ def main(src, cities_dest, rural_dest):
 
     rural = (midwest
         .filter(midwest.population < 100000)
-        .rdd.map(lambda t: clean_tweet(t.body))
+        .rdd.map(lambda t: Row(body=clean_tweet(t.body)))
         .toDF()
         .coalesce(100))
 
     urban = (midwest
         .filter(midwest.population > 100000)
         .filter(midwest.population < 1000000)
-        .rdd.map(lambda t: clean_tweet(t.body))
+        .rdd.map(lambda t: Row(body=clean_tweet(t.body)))
         .toDF()
         .coalesce(100))
 
     cities.write.mode('overwrite').csv(cities_dest)
     rural.write.mode('overwrite').text(rural_dest)
-    urban.write.mode('overwrite').text(ruban_dest)
+    urban.write.mode('overwrite').text(urban_dest)
 
 
 if __name__ == '__main__':
